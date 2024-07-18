925cb6dfb463991f4b30956fd0354a453fbb5182
Zac Zou <68052080+Euchiz@users.noreply.github.com>
2024-07-18 15:26:08 -0400

    Delete MLR.R

--- MLR.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(readxl)
-
-wd <- "E:/zaczou/Research/Chen Lab/projects/TIMER/"
-rawfile <- "./2022-11-18_vp_titer.xls"
-datafile <- "./data.csv"
-stdfile <- "./stdcurve.csv"
-
-setwd(wd)
